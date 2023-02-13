ac20693dd133ed090d83545695651e9a63ec05fc
OsiriSPlayer <osirixplayer@gmail.com>
2023-02-13 11:01:32 +0100

    Actualizacion de proyecto

---
+++ EnoRegV2/EnoRegV2/Dominio/Lote.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnoregV2.Dominio
+{
+    internal class Lote
+    {
+        private 
+        private int idProducto;
+        private DateTime fechaCaducidad;
+        private double stock;
+
+        public Lote(int idProducto, DateTime fechaCaducidad, double stock)
+        {
+            this.idProducto = idProducto;
+            this.fechaCaducidad = fechaCaducidad;
+            this.stock = stock;
+        }
+
+        public int IdProducto { get => idProducto; set => idProducto = value; }
+        public DateTime FechaCaducidad { get => fechaCaducidad; set => fechaCaducidad = value; }
+        public double Stock { get => stock; set => stock = value; }
+    }
+}
---
+++ EnoRegV2/EnoRegV2/Dominio/Producto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnoregV2.Dominio
+{
+    public class Producto
+    {
+        private int id;
+        private string nombre;
+        private string unidad;
+        private Double stock;
+        private byte[] imagen;
+
+
+        public Producto() { 
+        
+        }
+        public Producto(int i,string nom,string un,Double st, byte[] image)
+        {
+            id = i;
+            nombre = nom;
+            unidad = un;
+            stock = st;
+            imagen = image;
+        }
+
+        public Producto(int i, string nom, string un, Double st)
+        {
+            id = i;
+            nombre = nom;
+            unidad = un;
+            stock = st;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Unidad { get => unidad; set => unidad = value; }
+        public double Stock { get => stock; set => stock = value; }
+        public byte[] Imagen 
+        { 
+            get => imagen; 
+            set => imagen = value; 
+        }
+
+        public Bitmap ByteArrayToImage(byte[] source)
+        {
+            using (var ms = new MemoryStream(source))
+            {
+                return new Bitmap(ms);
+            }
+        }
+        public void SetImagenAByte(Image img)
+        { 
+            ImageConverter converter = new ImageConverter();
+            imagen=(byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
+    }
+}
