0add461fece573c188a09d0f7a9be8f95e2e1760
Fermin Pozo <ferminpozo2k@gmail.com>
2023-02-20 19:09:59 +0100

    documentacion clase registro

--- EnoRegV2/EnoRegV2/Dominio/Registro.cs
+++ EnoRegV2/EnoRegV2/Dominio/Registro.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace EnoregV2.Dominio { 
     internal class Registro
     {
+        //declaracion de variables
         private string fecha;
         private Lote lote;
         private Double cantidad;
@@ -14,7 +15,20 @@ namespace EnoregV2.Dominio {
         private Double cantidadLote;
         private double cantidadProducto;
 
+        /// <summary>
+        /// Constructor vacio de la clase Registros <see cref="Registro"/> class.
+        /// </summary>
         public Registro() { }
+
+        /// <summary>
+        /// Constructor con parametros de la clase Registros <see cref="Registro"/> class.
+        /// </summary>
+        /// <param name="f"></param>
+        /// <param name="l"></param>
+        /// <param name="c"></param>
+        /// <param name="o"></param>
+        /// <param name="cantLote"></param>
+        /// <param name="cantProducto"></param>
         public Registro(string f,Lote l,Double c,string o,Double cantLote,double cantProducto) { 
             fecha= f;
             lote = l;
@@ -23,6 +37,8 @@ namespace EnoregV2.Dominio {
             cantidadLote= cantLote;
             cantidadProducto= cantProducto;
         }
+
+        //getters & setters
         public string Fecha { get => fecha; set => fecha = value; }
         public double Cantidad { get => cantidad; set => cantidad = value; }
         public string Observaciones { get => observaciones; set => observaciones = value; }
